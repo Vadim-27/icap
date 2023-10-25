a03cb0d1695624805bb6243788b0729228b6f22a
Vadim-27 <108513205+Vadim-27@users.noreply.github.com>
2023-10-25 17:35:34 +0000

    Deploying to gh-pages from @ Vadim-27/icap@321559cd1c029f202455cf4fe617e3f67c9eb153 🚀

---
+++ static/js/489.b715e07d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkassembly=self.webpackChunkassembly||[]).push([[489],{2489:function(e,a,t){t.r(a);var n=t(8546),p=t(184);a.default=function(){return(0,p.jsx)("div",{className:"".concat(n.Z.wrapper," container"),children:(0,p.jsx)("p",{children:"Not Found Page"})})}},8546:function(e,a){a.Z={wrapper:"page_wrapper__f-hWG",title:"page_title__pwan3",text:"page_text__9RBxz"}}}]);
+//# sourceMappingURL=489.b715e07d.chunk.js.map
